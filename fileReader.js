16d38d086c140791da3f1e9140ac1e37c785878c
kisstheair <fighting20xx@outlook.com>
2017-08-07 22:49:57 +0800

    s

---
+++ fileReader.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator on 2017/8/7.
+ */
+
+var fs = require('fs');
+var util = require('util');
+var path = './image/';
+
+function explorer(path){
+    fs.readdir(path, function(err, files){
+        //err 为错误 , files 文件名列表包含文件夹与文件
+        if(err){
+            console.log('error:\n' + err);
+            return;
+        }
+
+        files.forEach(function(file){
+
+            fs.stat(path + '/' + file, function(err, stat){
+                if(err){console.log(err); return;}
+                if(stat.isDirectory()){
+                    // 如果是文件夹遍历
+                    explorer(path + '/' + file);
+                }else{
+                    // 读出所有的文件
+                    console.log('<img src=\"' + path + '' + file+"\">");
+                }
+            });
+
+        });
+
+    });
+}
+
+
+explorer(path);
